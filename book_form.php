d32f0bc2e86e3a2d38df5b1c5bbe1af3f997ce7a
ngelkyutie <ngelsarandin@gmail.com>
2024-12-01 22:38:58 +0800

    book_form.php

--- book_form.php
+++ book_form.php
@@ -1,36 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
 
-$connection = mysqli_connect(‘localhost’,’root’,’’,’book_db’);
+	$connection = mysqli_connect('localhost', 'root', '', 'book_db');
 
-if(isset($_POST[‘send’])){
-$name = $_POST[‘name’];
-$email = $_POST[‘email’];
-$phone = $_POST[‘phone’];
-$address = $_POST[‘address’];
-$location = $_POST[‘location’];
-$guests = $_POST[‘guests’];
-$arrivals = $_POST[‘arrivals’];
-$leaving = $_POST[‘leaving’];
 
-$request = “ insert into book_form(name, email, phone, address, location, guests, arrivals,	leaving) values (‘$name’, ‘$email’, ‘$phone’, ‘$address’, ‘$location’, ‘$guests’, ‘$arrivals’, ‘$leaving’) ”;
+	if(isset($_POST['send'])){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$location = $_POST['location'];
+$guests = $_POST['guests'];
+$arrivals = $_POST['arrivals'];
+$leaving = $_POST['leaving'];
 
-mysqli_query($connection, $request);
-
-header(‘location:book.php’);
+$stmt = $connection->prepare("INSERT INTO book_form (name, email, phone, address, location, guests, arrivals, leaving) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssssiis", $name, $email, $phone, $address, $location, $guests, $arrivals, $leaving);
+if ($stmt->execute()) {
+    header('Location: book.php');
+} else {
+    echo "⚠ Something went wrong. Please try again.";
 }
-else{
-echo ‘⚠ Something went Wrong, Please! try again.’;
-}
-
+    }
 ?>
-
-</body>
-</html>
